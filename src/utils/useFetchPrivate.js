fbf0467d021082e9a9adea89893c38f0485d35d0
tiw_13 <samarn.sae@gmail.com>
2024-03-06 15:17:48 +0700

    Update useFetchPrivate.js

--- src/utils/useFetchPrivate.js
+++ src/utils/useFetchPrivate.js
@@ -1,11 +1,15 @@
+// file: ./utils/useFetchPrivate
+
 import { useState } from "react";
 import { useAuth } from "./AuthProvider";
-
+// import { _fetch as fetch} from './fetchWTimeout'
 let useFetchPrivate = () => {
   const { user, setUser } = useAuth();
   const [loading, setLoading] = useState(true);
 
-  const setAuthorizationHeader = (params, accessToken) => {
+  //    const baseURL = 'http://127.0.0.1:4000'
+
+  const setAuthorizationHeader = (params, accessToken = user.accessToken) => {
     if (!params) {
       params = {
         credentials: "include",
@@ -15,81 +19,81 @@ let useFetchPrivate = () => {
         },
       };
     } else {
-      if (!params.headers || !params.headers["Authorization"]) {
+      if (!params.headers || accessToken) {
         Object.assign(params, {
           headers: { Authorization: `Bearer ${accessToken}` },
         });
       }
       Object.assign(params, { credentials: "include" });
-      params.headers = {
-        ...params.headers,
-        "Content-Type": "application/json",
-      };
+      if (!params.noContentType) {
+        Object.assign(params.headers, { "Content-Type": "application/json" });
+      } else if (params.noContentType && params.headers["Content-Type"]) {
+        params.headers.delete("Content-Type");
+      }
     }
     return params;
   };
 
   const originalRequest = async (url, config) => {
-    config = setAuthorizationHeader(config, user.accessToken);
     let response = await fetch(url, config);
     return response;
   };
 
   const refreshToken = async () => {
-    try {
-      const response = await fetch(
-        "https://api-data-medical-room-tu.onrender.com/auth/refresh",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        setUser((prev) => {
-          return { ...prev, accessToken: data.accessToken };
-        });
-        return data.accessToken;
-      } else {
-        const error = new Error("Fail to refresh! while fetching the data.");
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-          error.info = await response.json();
-        } else {
-          error.info = response.statusText;
-        }
-        error.status = response.status;
-        throw error;
+    let response = await fetch(
+      "https://api-data-medical-room-tu.onrender.com/auth/refresh",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    } catch (error) {
-      console.error("Error refreshing token:", error);
+    );
+    if (response.ok) {
+      let data = await response.json();
+      let tempUser = { ...user, accessToken: data.accessToken };
+      sessionStorage.setItem("aToken", JSON.stringify(tempUser)); // add this forget to set the session storage
+      setUser(tempUser);
+      return data.accessToken;
+    } else {
+      const error = new Error("Fail to refresh! while fetching the data.");
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1)
+        error.info = await response.json();
+      else error.info = await response.text();
+      error.status = response.status;
+      //      signout(()=> console.log("Fail to refresh! force signout!"));
       throw error;
     }
+  };
+  const processResult = async (response) => {
+    let data = {};
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") >= 0) {
+      data = await response.json();
+    } else if (contentType && contentType.indexOf("text/") >= 0) {
+      data = await response.text();
+    } else data = await response.blob(); // expect it is a mimetype data i.e., image
+    return { response, data };
   };
 
   const callFetch = async (url, config) => {
     try {
-      let response = await originalRequest(url, config);
-
+      //      let newConfig = setAuthorizationHeader(config, user.accessToken);
+      let newConfig = setAuthorizationHeader(config);
+      let response = await originalRequest(url, newConfig);
       if (response.ok) {
-        let data = await response.json();
-        return { response, data };
+        return processResult(response);
+        // let data = await response.json();
+        // return {response, data};
       } else {
         if (response?.status === 403) {
           // token expire or loss?
           const newAccessToken = await refreshToken();
-          const options = setAuthorizationHeader(
-            config,
-            `Bearer ${newAccessToken}`
-          );
+          const options = setAuthorizationHeader(config, newAccessToken);
           let response = await originalRequest(url, options);
-          let data = {};
           if (response.ok) {
-            data = await response.json();
-            return { response, data };
+            return processResult(response);
           } else {
             throw new Error("Request fail!");
           }
